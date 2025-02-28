b71e34184a5df10ae09e8555f997863e998ba51c
dataemperor <tjchankamafernando@gmail.com>
2025-02-28 19:45:38 +0530

    basic footer

--- Shrimple/src/App.tsx
+++ Shrimple/src/App.tsx
@@ -1,12 +1,13 @@
 import Navbar from "./components/Navbar";
 import {BrowserRouter as Router} from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <Router>
       <Navbar/>
-      <AppRoutes/> 
+      <AppRoutes/>
+      <Footer/>   
     </Router>
   );
 }
